a5b4b948eed669cbc2b6dfc4734756acb2b721be
mustafamajeed276 <mmustafamajeed1@gmail.com>
2025-04-29 12:31:05 +0500

    Coun/ting till 10

---
+++ Counting.java
@@ -0,0 +1,13 @@
+class Counting {
+    public static void main(String[] args) {
+        for(int i=10; i>1; i--){
+            System.out.println("The value of i is "+i);
+        }
+
+        int j=0;
+        while(j<11) {
+            System.out.println("THe value of i is "+j);
+            j++;
+        }
+    }
+}
