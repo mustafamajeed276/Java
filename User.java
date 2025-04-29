9927d5dc98cd5d9a447cc68bbec39e46e078c98e
mustafamajeed276 <mmustafamajeed1@gmail.com>
2025-04-29 12:18:27 +0500

    User Information

---
+++ User.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class User {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Hello user please enter your name: ");
+        String name=sc.nextLine();
+        System.out.println("Enter your lucky number: ");
+        int Lucky=sc.nextInt();
+        System.out.println("Hello "+name +", Your lucky number "+Lucky +" show you are reall7 hardworking keep it up");
+    }
+}
