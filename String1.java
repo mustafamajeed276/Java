63cfd1a93a9f57497f447c0710a822b6e7aaef24
mustafamajeed276 <mmustafamajeed1@gmail.com>
2025-04-30 12:47:20 +0500

    String1

---
+++ String1.java
@@ -0,0 +1,21 @@
+class String1 {
+    public static void main(String[] args){
+        String first = "Cod";
+        String second = "ingal";
+        String coding = first+second;
+        String codingTrick = "Welcome"+"to"+"Codingal";
+        String codingalCapital = coding.toUpperCase();
+        String codingalLower = coding.toLowerCase();
+        
+        int LenghtOfCodingal=coding.length();
+        int LeghtOfCodingalTrick=codingTrick.lenght();
+        int sum = LengthOfCodingal + LegthOfCodingalTrick;
+
+        
+        System.out.println(coding);
+        System.out.println(codingTrick);
+        System.out.println(codingalCapital);
+        System.out.println(codingalLower);
+        System.out.println(sum);
+    }
+}
