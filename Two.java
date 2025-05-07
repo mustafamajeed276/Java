9cfb79c7a5f2b4fb2e4e621f27fcc7557b585bd0
mustafamajeed276 <mmustafamajeed1@gmail.com>
2025-05-07 11:25:39 +0500

    calculator

--- Two.java
+++ Two.java
@@ -1,26 +1,28 @@
-class Mammals{
-    void mam(){
-        System.out.println("Inside mammals class");
+
+class Mammals {
+    void mam() {
+        System.out.println("Inside Mammals Class");
     }
 }
 
-class Lion extends Mammals{
-    void roar(){
-        System.out.println("Inside Lion class");
+// child class
+class Lion extends Mammals {
+    void roar() {
+        System.out.println("Inside Lion class ");
     }
 }
 
-class Human extends Mammals{
-    void hum(){
+// child
+class Human extends Mammals {
+    void hum() {
         System.out.println("Inside Human");
     }
 }
 
-class Two{
-    public static void main(String args[]){
-        Lion obj=new Lion();
-        obj.hum();
+class Two {
+    public static void main(String args[]) {
+        Lion obj = new Lion();
         obj.roar();
-        obj.mam();        
+        obj.mam();
     }
 }
