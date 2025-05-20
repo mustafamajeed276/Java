b90b78d158c8487a584297e6876fbdc8c732938e
mustafamajeed276 <mmustafamajeed1@gmail.com>
2025-05-20 11:31:38 +0500

    interface

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Dream is the best minecraft player");
+interface MyInterFace
+{
+    public void method1();
+    public void method2();
+}
+class Main implements MyInterFace
+{
+    public void method1()
+    {
+        System.out.println("Implementation of method1");
+    }
+    public void method2()
+    {
+        System.out.println("Implementation of method2");
+    }
+    public static void main(String args[])
+    {
+        MyInterFace obj = new Main();
+        obj.method1();
+        obj.method2();
     }
 }
